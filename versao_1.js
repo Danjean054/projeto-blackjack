7a3d07adaaf2a697c3ea9abee7efae5bf97b0df9
Daniel Weslley Silva Xavier <danielweslley882@gmail.com>
2023-05-29 16:10:03 -0300

    entrega-da-primeira-parte-do-blackJack

--- versao_1.js
+++ versao_1.js
@@ -10,3 +10,45 @@
  * 
  * 
  */
+
+    console.log("Boas vindas ao jogo de BlackJack");
+    const iniciarJogo =  confirm("Quer iniciar uma nova rodada ?")
+
+   if (iniciarJogo === true){
+
+      console.log("iniciar uma rodada");
+   }else{
+
+      console.log("O jogo acabou");
+   }
+
+   //const carta = comprarCarta();
+
+   const usuario = comprarCarta()
+
+   const usuario2 = comprarCarta()
+
+   let soma = Number(usuario.valor + usuario2.valor)
+
+   const computador = comprarCarta()
+
+   const computador2 = comprarCarta()
+
+   let soma2 = Number(computador.valor + computador2.valor)
+
+   console.log(`Usuário cartas: ${usuario.texto} ${usuario2.texto}  pontuação ${soma}`);
+
+   console.log(`Computador cartas: ${computador.texto} ${computador2.texto},pontuação ${soma2}`);
+
+   if (soma > soma2 && soma <= 21) {
+      
+      console.log("Usuário ganhou");
+   } else if (soma < soma2 && soma2 <= 21) {
+
+      console.log("Computador ganhou");
+         
+   }else {
+
+      console.log("Empate");
+      
+   }
