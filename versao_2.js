eac25f8996fbc6c02c6910d68d8c6aca0b0e6a3a
Daniel Weslley Silva Xavier <danielweslley882@gmail.com>
2023-06-19 17:55:10 -0300

    projeto finalizado parte dois

--- versao_2.js
+++ versao_2.js
@@ -10,181 +10,166 @@
  * 
  * 
  */
-        let iniciarJogo =  confirm("Boas vindas ao jogo de BlackJack \n Quer iniciar uma nova rodada ?")
-   if (iniciarJogo) {
-      
-   
-    let usuario = []
+let iniciarJogo = confirm("Boas vindas ao jogo de BlackJack \n Quer iniciar uma nova rodada ?")
+if (iniciarJogo) {
+
+
+  let usuario = []
+
+  usuario.push(comprarCarta())
+
+  usuario.push(comprarCarta())
+
+  let valorDoUsuario = usuario[0].valor
+  let valorDoUsuario2 = usuario[1].valor
+  let somaDoUsuario = valorDoUsuario + valorDoUsuario2
+
+
+  let computador = []
+
+  computador.push(comprarCarta())
+
+  computador.push(comprarCarta())
+
+  let valorDoComputador = computador[0].valor
+  let valorDoComputador2 = computador[1].valor
+  let somaDoComputador = valorDoComputador + valorDoComputador2
+
+  if (usuario[0].valor === 11 && usuario[1].valor === 11) {
+
+    usuario = []
 
     usuario.push(comprarCarta())
 
     usuario.push(comprarCarta())
 
-    let valorDoUsuario = usuario[0].valor
-    let valorDoUsuario2 = usuario[1].valor
-    let soma = valorDoUsuario  + valorDoUsuario2
+  }
+  if (computador[0].valor === 11 && computador[1].valor === 11) {
 
-        
-    let computador = []
-    
-    computador.push(comprarCarta())
+    computador = []
 
     computador.push(comprarCarta())
 
-    let valorDoComputador = computador[0].valor
-    let valorDoComputador2 = computador[1].valor
-    let soma2 = valorDoComputador  + valorDoComputador2
+    computador.push(comprarCarta())
 
-    if (usuario[0].valor === 11 && usuario[1].valor === 11 ) {
-      
-       usuario = []
-     
-       usuario.push(comprarCarta())
+  }
 
-       usuario.push(comprarCarta())
+  let comprarCartaDoJogo = confirm(`Suas cartas são ${usuario[0].texto} ${usuario[1].texto}. A carta revelada do computador é ${computador[0].texto}
+      Deseja comprar mais cartas ?`)
 
-       computador = []
+  while (comprarCartaDoJogo) {
 
-       computador.push(comprarCarta())
 
-       computador.push(comprarCarta())
+    guardarTextoDoUsuario = ""
+
+    guardarValorDoUsuario = 0
+
+    guardarTextoDoComputador = ""
+
+    guardarValorDoComputador = 0
+
+
+    usuario.push(comprarCarta())
+
+
+    for (const i of usuario) {
+
+
+      guardarValorDoUsuario += i.valor
+
+      guardarTextoDoUsuario += i.texto
 
     }
-      if (computador[0].valor === 11 && computador[1].valor === 11) {
+    for (const carta of computador) {
 
-         computador = []
 
-         computador.push(comprarCarta())
+      guardarValorDoComputador += carta.valor
 
-         computador.push(comprarCarta())
+      guardarTextoDoComputador += carta.texto
 
-       usuario = []
-     
-       usuario.push(comprarCarta())
+    }
+    comprarCartaDoJogo = confirm(`Suas cartas são ${guardarTextoDoUsuario}. A carta revelada do computador é ${computador[0].texto}
+           Deseja comprar mais cartas ?`)
 
-       usuario.push(comprarCarta())
-                 
-      }      
-   
-     let comprarCartaDoJogo = confirm(`Suas cartas são ${usuario[0].texto} ${usuario[1].texto}. A carta revelada do computador é ${computador[0].texto }
-      Deseja comprar mais cartas ?`)
-      
-         while (comprarCartaDoJogo) {
-                
-         
-            guardarTextoDoUsuario = ""
 
-            guardarValorDoUsuario = 0
-      
-            guardarTextoDoComputador = ""
-      
-            guardarValorDoComputador = 0
-           
-              
-        usuario.push(comprarCarta())
-        
+    if (guardarValorDoUsuario > 21) {
 
-        for (const i of usuario) {
-         
-        
-          guardarValorDoUsuario += i.valor
-       
-          guardarTextoDoUsuario += i.texto
-          
-          }
-          for (const carta of computador) {
-         
-        
-            guardarValorDoComputador += carta.valor
-         
-            guardarTextoDoComputador += carta.texto
-            
-            }
-             comprarCartaDoJogo = confirm(`Suas cartas são ${guardarTextoDoUsuario}. A carta revelada do computador é ${computador[0].texto}
-           Deseja comprar mais cartas ?`) 
-                    
-            
-           if(guardarValorDoUsuario > 21){
+      alert(`Usuário - cartas:${guardarTextoDoUsuario} - pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\nComputador ganhou`)
+      break
+    } if (comprarCartaDoJogo === false) {
 
-            alert(`Usuário - cartas:${guardarTextoDoUsuario} - pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\nComputador ganhou`)
-           break
-         }if(comprarCartaDoJogo === false){
-           
-            if(guardarValorDoUsuario > guardarValorDoComputador && guardarValorDoUsuario <= 21){
-   
-             alert(`Usuário - cartas:${guardarTextoDoUsuario}- pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\n Usuário ganhou`)
-    
-          }else if(guardarValorDoUsuario < guardarValorDoComputador  && guardarValorDoComputador <= 21){
-    
-            alert(`Usuário - cartas:${guardarTextoDoUsuario} - pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\nComputador ganhou`)
-    
-          }else if (guardarValorDoComputador === guardarValorDoUsuario) {
-             
-          alert(`Usuário - cartas:${guardarTextoDoUsuario}- pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\n Empate`)
-          } 
-          break
-         }
-         
-       }  while (comprarCartaDoJogo === false && soma < soma2 ) {
-                
-         if (soma2 <= soma) {
-            
-         }
-         guardarTextoDoUsuario = ""
+      if (guardarValorDoUsuario > guardarValorDoComputador && guardarValorDoUsuario <= 21) {
 
-         guardarValorDoUsuario = 0
-   
-         guardarTextoDoComputador = ""
-   
-         guardarValorDoComputador = 0
-          
-        
-          computador.push(comprarCarta())
-                 
+        alert(`Usuário - cartas:${guardarTextoDoUsuario}- pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\n Usuário ganhou`)
 
-     for (const i of usuario) {
-      
-     
-       guardarValorDoUsuario += i.valor
-    
-       guardarTextoDoUsuario += i.texto
-       
-       }
-       for (const j of computador) {
-      
-     
-         guardarValorDoComputador += j.valor
-      
-         guardarTextoDoComputador += j.texto
-            
-       }  guardarValorDoComputador 
-         
-         if(guardarValorDoComputador > 21){
+      } else if (guardarValorDoUsuario < guardarValorDoComputador && guardarValorDoComputador <= 21) {
 
-          alert(`Usuário - cartas:${guardarTextoDoUsuario} - pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\n Usuário ganhou`)
-         break
+        alert(`Usuário - cartas:${guardarTextoDoUsuario} - pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\nComputador ganhou`)
+
+      } else if (guardarValorDoComputador === guardarValorDoUsuario) {
+
+        alert(`Usuário - cartas:${guardarTextoDoUsuario}- pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\n Empate`)
       }
-         if(comprarCartaDoJogo === false){  
-            
-            if(guardarValorDoUsuario > guardarValorDoComputador && guardarValorDoUsuario <= 21){
+      break
+    }
+
+  } while (comprarCartaDoJogo === false || (somaDoUsuario > somaDoComputador && somaDoUsuario < somaDoComputador) && somarDoComputador <= 21) {
+ 
    
-            alert(`Usuário - cartas:${guardarTextoDoUsuario}- pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\n Usuário ganhou`)
-   
-         }else if(guardarValorDoUsuario < guardarValorDoComputador  && guardarValorDoComputador <= 21){
-   
-           alert(`Usuário - cartas:${guardarTextoDoUsuario} - pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\nComputador ganhou`)
-   
-         }else if (guardarValorDoComputador === guardarValorDoUsuario) {
-            
-         alert(`Usuário - cartas:${guardarTextoDoUsuario}- pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\n Empate`)
-         } 
-         break
+    guardarTextoDoUsuario = ""
+
+    guardarValorDoUsuario = 0
+
+    guardarTextoDoComputador = ""
+
+    guardarValorDoComputador = 0
+
+
+    computador.push(comprarCarta())
+
+
+    for (const i of usuario) {
+
+
+      guardarValorDoUsuario += i.valor
+
+      guardarTextoDoUsuario += i.texto
+
+    }
+    for (const j of computador) {
+
+
+      guardarValorDoComputador += j.valor
+
+      guardarTextoDoComputador += j.texto
+
+    }
+
+    if (guardarValorDoComputador > 21) {
+
+      alert(`Usuário - cartas:${guardarTextoDoUsuario} - pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\n Usuário ganhou`)
+      break
+    }
+    if (comprarCartaDoJogo === false) {
+
+      if (guardarValorDoUsuario > guardarValorDoComputador && guardarValorDoUsuario <= 21) {
+
+        alert(`Usuário - cartas:${guardarTextoDoUsuario}- pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\n Usuário ganhou`)
+
+      } else if (guardarValorDoUsuario < guardarValorDoComputador && guardarValorDoComputador <= 21) {
+
+        alert(`Usuário - cartas:${guardarTextoDoUsuario} - pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\nComputador ganhou`)
+
+      } else if (guardarValorDoComputador === guardarValorDoUsuario) {
+
+        alert(`Usuário - cartas:${guardarTextoDoUsuario}- pontuação ${guardarValorDoUsuario}\nComputador - cartas:  ${guardarTextoDoComputador} - pontuação ${guardarValorDoComputador}\n Empate`)
       }
-        
-      }
-   }else{
-         alert(`O jogo acabou`)
-      }
-      
-      
-                 
+      break
+    }
+  }
+} else {
+  alert(`O jogo acabou`)
+}
+
+
+
